1e15aef030c2f8ed4741c81e6fb0d8cd156958df
MarioLeonCarrizo <mlc@msistudio.com>
2024-04-16 14:33:40 +0200

    Mejoras

--- Ex.Core/Commands/ViewInformationCADCommand.cs
+++ Ex.Core/Commands/ViewInformationCADCommand.cs
@@ -7,11 +7,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 using System.Windows.Documents;
 using System.Windows.Media;
 using System.Windows.Media.TextFormatting;
 using System.Windows.Shapes;
 using System.Xaml;
+using static System.Windows.Forms.LinkLabel;
 using Line = Autodesk.Revit.DB.Line;
 
 namespace Ex.Core
@@ -59,7 +61,6 @@ namespace Ex.Core
                 if (importedCAD.Location is LocationCurve locationCurve)
                 {
                     Curve curve = locationCurve.Curve;
-                    Line line = curve as Line;
                     // Accedemos a la informacion
                     XYZ startPoint = curve.GetEndPoint(0);
                     XYZ endPoint = curve.GetEndPoint(1);
@@ -70,10 +71,19 @@ namespace Ex.Core
                     endPoint = new XYZ(Math.Round(endPoint.X, 3), Math.Round(endPoint.Y, 3), Math.Round(endPoint.Z, 3));
                     length = Math.Round(length, 3);
 
-                    InfoViews += $"Start Point: {{{startPoint.X,-6} - {startPoint.Y,-6}}}" +
-                                 $"\t\tEnd Point: {{{endPoint.X,-6} - {endPoint.Y,-6}}}" +
-                                 $"\t\tLenght: {length} " +
-                                 $"\t\tDirection: {line.Direction}\n";
+                    if (curve is Line line)
+                    {
+                        InfoViews += $"Line Wall: Start Point: {{{startPoint.X,-7}\t-\t{startPoint.Y,-7}}}  " +
+                                     $"\t  |\tEnd Point: {{{endPoint.X,-7}\t-\t{endPoint.Y,-7}}}  " +
+                                     $"\t  |\tLenght: {length}  " +
+                                     $"\t  |\tDirection: {Math.Round(line.Direction.X, 1)},{Math.Round(line.Direction.Y, 1)}\n";
+                    }
+                    else if(curve is Arc arc)
+                    {
+                        InfoViews += $"Line Arc: Start Point: {{{startPoint.X,-7}\t-\t{startPoint.Y,-7}}}  " +
+                                     $"\t  |\tEnd Point: {{{endPoint.X,-7}\t-\t{endPoint.Y,-7}}}  " +
+                                     $"\t  |\tLenght: {length} \n";
+                    }
                 }
             }
 
@@ -100,10 +110,12 @@ namespace Ex.Core
             return curves;
         }
 
-        List<Curve> SimplifyListCurves(List<Curve> lines)
+        List<Curve> SimplifyListCurves(List<Curve> xLines)
         {
             List<Curve> curves = new List<Curve>();
             HashSet<Curve> simplifiedCurves = new HashSet<Curve>();
+
+            List<Line> lines = xLines.OfType<Line>().ToList();
 
             for(int i = 0; i < lines.Count; i++)
             {
@@ -131,12 +143,13 @@ namespace Ex.Core
         }
 
         /// <returns><see cref="Line"/> paralela a <paramref name="origin"/></returns>
-        Line FindParalelLine(List<Curve> lines, int startIndex, Line origin)
+        Line FindParalelLine(List<Line> lines, int startIndex, Line origin)
         {
+            LineInfo infoOrigin = new LineInfo(origin);
             for (int i = startIndex; i < lines.Count; i++)
             {
-                Line line = lines[i] as Line;
-                if (IsClose(MidPos(line.GetEndPoint(0), line.GetEndPoint(1)), MidPos(origin.GetEndPoint(0), origin.GetEndPoint(1))) && AreParallel(origin, line))
+                Line line = lines[i];
+                if (IsClose(MidPos(line.GetEndPoint(0), line.GetEndPoint(1)), MidPos(infoOrigin.Start, infoOrigin.End)) && AreParallel(origin, line))
                     return line;
             }
             return null;
@@ -150,32 +163,35 @@ namespace Ex.Core
         /// <returns>Devuelve una <see cref="Line"/> entre dos lineas</returns>
         Line CreateCenteredLine(Line line1, Line line2)
         {
-            //Line1Points
-            XYZ line1Start = line1.GetEndPoint(0);
-            XYZ line1End = line1.GetEndPoint(1);
-            //Line2 Points
-            XYZ line2Start = line2.GetEndPoint(0);
-            XYZ line2End = line2.GetEndPoint(1);
+            LineInfo Info1 = new LineInfo(line1);
+            LineInfo Info2 = new LineInfo(line2);
 
             XYZ midPoint = MidPos(line1.Origin, line2.Origin);
 
             Line cl;
-            if (Math.Abs(line1.Direction.X) == 1) // Horizontal line
-            {
-                cl =  Line.CreateBound(new XYZ(MidCord(line1Start.X, line2End.X), midPoint.Y, 0),
-                                        new XYZ(MidCord(line1End.X, line2Start.X), midPoint.Y,   0));
-            }
-            else // Vertical line
-            {
-                cl = Line.CreateBound(new XYZ(midPoint.X, MidCord(line1Start.Y, line2End.Y), 0),
-                                        new XYZ(midPoint.X, MidCord(line1End.Y, line2Start.Y),   0));
-            }
+            cl = Line.CreateBound(new XYZ(MidCord(Info1.Left, Info2.Left), MidCord(Info1.Up, Info2.Up), 0),
+                                  new XYZ(MidCord(Info1.Right, Info2.Right), MidCord(Info1.Down, Info2.Down), 0));
+            //if (Math.Abs(Math.Round(line1.Direction.X, 1)) == 1f) // Horizontal line
+            //{
+            //    cl = Line.CreateBound(new XYZ(MidCord(Info1.Left, Info2.Left), midPoint.Y, 0),
+            //                           new XYZ(MidCord(Info1.Right, Info2.Right), midPoint.Y, 0));
+            //}
+            //else if (Math.Abs(Math.Round(line1.Direction.Y, 1)) == 1f) // Vertical line
+            //{
+            //    cl = Line.CreateBound(new XYZ(midPoint.X, MidCord(Info1.Up, Info2.Up), 0),
+            //                          new XYZ(midPoint.X, MidCord(Info1.Down, Info2.Down), 0));
+            //}
+            //else
+            //{
+            //    cl = Line.CreateBound(new XYZ(MidCord(Info1.Left, Info2.Left), MidCord(Info1.Up, Info2.Up), 0),
+            //                          new XYZ(MidCord(Info1.Right, Info2.Right), MidCord(Info1.Down, Info2.Down), 0));
+            //}
 
-            return Line.CreateBound(cl.GetEndPoint(0) - (cl.Direction/10),
-                                    cl.GetEndPoint(1) + (cl.Direction/10));
+            return Line.CreateBound(cl.GetEndPoint(0) - (cl.Direction/20),
+                                    cl.GetEndPoint(1) + (cl.Direction/20));
         }
 
-        //---- Calcular Parametros ----//
+        //-------- Calcular Parametros --------//
         /// <returns><see langword="true"/> en caso de que las dos lineas sean paralelas</returns>
         bool AreParallel(Line line1, Line line2) => Math.Abs(Math.Abs(line1.Direction.DotProduct(line2.Direction)) - 1) < 1;
         /// <returns><see langword="true"/> en caso de que los dos puntos esten cerca</returns>
@@ -184,7 +200,7 @@ namespace Ex.Core
         XYZ MidPos(XYZ a, XYZ b) => (a + b) / 2;            //Saber la media entre dos posiciones
         /// <returns> La media entre dos variables</returns>
         double MidCord(double a, double b) => (a + b) / 2;  //Saber la media entre dos variables
-        //----------------------------//
+        //-------------------------------------//
 
 
         public void CreateWalls(Document doc, List<Curve> curves, WallType wallType, Level level)
@@ -238,39 +254,25 @@ namespace Ex.Core
         public static string GetPath() => typeof(ViewInformationCADCommand).Namespace + "." + nameof(ViewInformationCADCommand);
     }
 
-    class CombinationLines
+    class LineInfo
     {
-        List<Line> Lines = new List<Line>();
-        bool IsClosed;
+        public XYZ Start;
+        public XYZ End;
 
-        public CombinationLines(List<Line> lines, bool isClosed)
+        public double Up;
+        public double Down;
+        public double Left;
+        public double Right;
+
+        public LineInfo(Line line)
         {
-            Lines = lines;
-            IsClosed = isClosed;
-        }
+            Start = line.GetEndPoint(0);
+            End = line.GetEndPoint(1);
 
-        public List<XYZ> SimplifiedPoints()
-        {
-            List<XYZ> newPoints = new List<XYZ>();
-
-            for (int i = 0; i < Lines.Count; i++)
-            {
-                Line currentLine = Lines[i];
-                Line nextLine = Lines[i + 1];
-
-                XYZ newPoint;
-                XYZ offset;
-
-                if (currentLine.Direction.X != 0)
-                    offset = new XYZ(currentLine.Direction.X * 0.5f, nextLine.Direction.Y * 0.5f, 0);
-                else
-                    offset = new XYZ(nextLine.Direction.X * 0.5f, currentLine.Direction.Y * 0.5f, 0);
-
-                newPoint = currentLine.GetEndPoint(1) + offset;
-                newPoints.Add(newPoint);
-            }
-
-            return newPoints;
+            Up = Math.Max(Start.Y, End.Y);
+            Down = Math.Min(Start.Y, End.Y);
+            Left = Math.Min(Start.X, End.X);
+            Right = Math.Max(Start.X, End.X);
         }
     }
 }
